559d982e300bcac1fd3b0d6a107f4108f1cd28b8
Renu Joshi <renujoshi0512@gmail.com>
2021-04-22 12:26:52 +0530

    updated userdriver

--- src/register/UserDriver.java
+++ src/register/UserDriver.java
@@ -17,33 +17,43 @@ public class UserDriver {
 	User user;
 	PreparedStatement pre;
 	Scanner sc;
-	
+	WelcomeGUI w;
+
+	public void say() {
+		System.out.println("Im in user driver class");
+	}
 	
 	
 	public UserDriver() {
 		this.con = DBConnect.con;
 		user = new User();
 		sc=new Scanner(System.in);
+		w=new WelcomeGUI();
+		w.setUserD(this);
 	}
 	
-	public void delete() {
-		System.out.println("Enter user_id to delete the user");
-		String userId = sc.next();
+	public boolean delete(String userId) {
+		boolean done = false;
 		
 		try {
 			pre = con.prepareStatement("delete from user where user_id= ?");
 			pre.setString(1, userId);
 			int ra=pre.executeUpdate();
-			if(ra>0)
+			if(ra>0) {
 				System.out.println("User Deleted Successfully..");
-			else
+				done = true;
+			}
+			else {
 				System.out.println("User was not deleted..");
+				done = false;
+			}
 			pre.close();
 			sc.close();
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		return done;
 	}
 	
 	public int insert()
@@ -51,47 +61,47 @@ public class UserDriver {
 		try
 		{
 			
-			System.out.println("Enter First Name of User");
-			user.setFirst_name(sc.next());
-			System.out.println("Enter Last Name of User");
-			user.setLast_name(sc.next());
-			System.out.println("Enter dob of User");
-			user.setDob(sc.next());
-			System.out.println("Enter Gender of User");
-			user.setGender(sc.next().charAt(0));
-			System.out.println("Enter Street of User");
-			user.setStreet(sc.next());
-			System.out.println("Enter Location of User");
-			user.setLocation(sc.next());
-			System.out.println("Enter City of User");
-			user.setCity(sc.next());
-			System.out.println("Enter State of User");
-			user.setState(sc.next());
-			System.out.println("Enter Mobile of User");
-			user.setMob_no(sc.next());
-			System.out.println("Enter Email Id of User");
-			user.setEmail(sc.next());
-			System.out.println("Enter UserType of User");
-			user.setUserType(sc.next());			
-			System.out.println("Enter Password for User");
-			user.setPassword(sc.next());
-			System.out.println("Enter Zipcode of User");
-			user.setZipcode(sc.nextLong());
+//			System.out.println("Enter First Name of User");
+//			user.setFirst_name(sc.next());
+//			System.out.println("Enter Last Name of User");
+//			user.setLast_name(sc.next());
+//			System.out.println("Enter dob of User");
+//			user.setDob(sc.next());
+//			System.out.println("Enter Gender of User");
+//			user.setGender(sc.next().charAt(0));
+//			System.out.println("Enter Street of User");
+//			user.setStreet(sc.next());
+//			System.out.println("Enter Location of User");
+//			user.setLocation(sc.next());
+//			System.out.println("Enter City of User");
+//			user.setCity(sc.next());
+//			System.out.println("Enter State of User");
+//			user.setState(sc.next());
+//			System.out.println("Enter Mobile of User");
+//			user.setMob_no(sc.next());
+//			System.out.println("Enter Email Id of User");
+//			user.setEmail(sc.next());
+//			System.out.println("Enter UserType of User");
+//			user.setUserType(sc.next());			
+//			System.out.println("Enter Password for User");
+//			user.setPassword(sc.next());
+//			System.out.println("Enter Zipcode of User");
+//			user.setZipcode(sc.nextLong());
 
 			pre=con.prepareStatement("insert into user(first_name, last_name, dob, gender, street, location, city, state, zipcode, mobile_no, email)  values(?,?,?,?,?,?,?,?,?,?,?)");
 
-			
-			pre.setString(1, user.getFirst_name());
-			pre.setString(2,user.getLast_name());
-			pre.setString(3,user.getDob());
-			pre.setString(4,String.valueOf(user.getGender()));
-			pre.setString(5,user.getStreet());
-			pre.setString(6,user.getLocation());
-			pre.setString(7,user.getCity());
-			pre.setString(8,user.getState());
-			pre.setLong(9,user.getZipcode());
-			pre.setString(10,user.getMob_no());
-			pre.setString(11,user.getEmail());
+			pre.setString(1, w.getSignUpGUI().getFirstName());
+			pre.setString(2,w.getSignUpGUI().getLastName());
+			pre.setString(3,w.getSignUpGUI().getDob());
+			pre.setString(4,w.getSignUpGUI().getGender());
+			pre.setString(5,w.getSignUpGUI().getStreet());
+			pre.setString(6,w.getSignUpGUI().get_location());
+			pre.setString(7,w.getSignUpGUI().getCity());
+			pre.setString(8,w.getSignUpGUI().get_state());
+			pre.setLong(9,w.getSignUpGUI().getZipcode());
+			pre.setString(10,w.getSignUpGUI().getMobile());
+			pre.setString(11,w.getSignUpGUI().getEmail());
+		
 			
 			int ra=pre.executeUpdate();
 			
@@ -100,8 +110,8 @@ public class UserDriver {
 			if(ra>0) {
 				System.out.println("User Details Committed..");
 				
+				String query = "select user_id from user where email="+"\""+w.getSignUpGUI().getEmail()+"\"";
 				
-				String query = "select user_id from user where email="+"\""+user.getEmail()+"\"";
 				Statement st = con.createStatement();
 				ResultSet rs = st.executeQuery(query);
 				
@@ -109,8 +119,8 @@ public class UserDriver {
 				
 				if(rs.next())
 					pre.setString(1, rs.getString("user_id"));
-				pre.setString(2, user.getPassword());
-				pre.setString(3, user.getUserType());
+				pre.setString(2, w.getSignUpGUI().getPassword());
+				pre.setString(3, "A");
 				pre.setInt(4, 0);
 				
 				int ra1 = pre.executeUpdate();
@@ -154,7 +164,42 @@ public class UserDriver {
 											 "invalid";
 								
 	}
-	
+	public boolean updateUser(String userId) {
+		boolean done = false;
+
+		try {
+			pre = con.prepareStatement("update user set first_name = ?, last_name = ?, dob = ?, gender = ?, street = ?, location = ?, city = ?, state = ?, zipcode = ?, mobile_no = ?, email = ? where user_id= ?");
+			// Uncomment when done with GUI
+
+//			pre.setString(1, getFirstName());
+//			pre.setString(2, getLastName());
+//			pre.setString(3, getDob());
+//			pre.setString(4, getGender());
+//			pre.setString(5, getStreet());
+//			pre.setString(6, get_location());
+//			pre.setString(7, getCity());
+//			pre.setString(8, get_state());
+//			pre.setString(9, getZipcode());
+//			pre.setString(10, getMobile());
+//			pre.setString(11, getEmail());
+			pre.setString(12, userId);
+			int ra=pre.executeUpdate();
+			if(ra>0) {
+				System.out.println("Users details updated successfully..");
+				done = true;
+			}
+			else {
+				System.out.println("Users details are not updated");
+				done = false;
+			}
+			pre.close();
+			sc.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return done;
+	}
 	
 	public static void main(String[] args) {
 		UserDriver u = new UserDriver();
