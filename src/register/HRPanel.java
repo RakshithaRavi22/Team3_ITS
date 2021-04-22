08bc2a8479f7c521df302856198d8c02b050ea4c
Renu Joshi <renujoshi0512@gmail.com>
2021-04-22 11:10:49 +0530

    spellerror

---
+++ src/register/HRPanel.java
@@ -0,0 +1,23 @@
+package register;
+
+public class HRPanel {
+
+	// private String empHRId; AUTO GENERATED - USERID
+	private String empHRName;
+	
+public HRPanel(String empHRName) {
+		super();
+		this.empHRName = empHRName;
+	}
+
+
+public String getEmpHRName() {
+	return empHRName;
+}
+
+public void setEmpHRName(String empHRName) {
+	this.empHRName = empHRName;
+}
+
+
+}
---
+++ src/register/TechPanel.java
@@ -0,0 +1,30 @@
+package register;
+
+public class TechPanel {
+	
+	// private String techId; AUTO GENERATED USERID 
+	private String techName;
+	private String Subjects;
+	
+	public TechPanel(String techName, String subjects) {
+		super();
+		this.techName = techName;
+		Subjects = subjects;
+	}
+	
+	public String getTechName() {
+		return techName;
+	}
+	public String getSubjects() {
+		return Subjects;
+	}
+	public void setTechName(String techName) {
+		this.techName = techName;
+	}
+	public void setSubjects(String subjects) {
+		Subjects = subjects;
+	}
+	
+	
+
+}
